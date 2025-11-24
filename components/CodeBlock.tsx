aa4b8807f3ed79c23c1c7812c5a74f544e436dfa
aboutexo04 <aboutexo04@gmail.com>
2025-11-24 17:30:57 +0900

    Correct codeblock

--- components/CodeBlock.tsx
+++ components/CodeBlock.tsx
@@ -11,20 +11,20 @@ const CodeBlock: React.FC<CodeBlockProps> = ({ code, className }) => {
   const lines = code.replace(/\r\n/g, '\n').replace(/\r/g, '\n').split('\n');
 
   return (
-    <pre className={`font-mono text-sm leading-6 overflow-x-auto p-4 ${className}`}>
-      <code>
+    <div className={`font-mono text-sm leading-6 overflow-x-auto p-4 ${className}`}>
+      <div className="min-w-max space-y-0.5">
         {lines.map((line, i) => (
-          <div key={i} className="table-row">
-            <span className="table-cell text-gray-600 select-none text-right pr-4 w-8 border-r border-gray-700/50 mr-4">
+          <div key={i} className="flex">
+            <span className="text-gray-600 select-none text-right pr-4 w-8 border-r border-gray-700/50">
               {i + 1}
             </span>
-            <span className="table-cell pl-4 whitespace-pre text-gray-200">
+            <span className="pl-4 whitespace-pre text-gray-200 flex-1">
               {line || ' '}
             </span>
           </div>
         ))}
-      </code>
-    </pre>
+      </div>
+    </div>
   );
 };
 
